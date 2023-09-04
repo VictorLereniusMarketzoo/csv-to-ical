404d649579b5e6a23c8a8c40d038528592969065
Victor Lerenius <victor.lerenius@marketzoo.com>
2023-09-04 20:58:52 +0200

    Initial commit

--- services/nodejs/app/webserver/controllers/api/csv.js
+++ services/nodejs/app/webserver/controllers/api/csv.js
@@ -13,21 +13,27 @@ module.exports = {
         }
 
         /* DOWNLOAD CSV */
-        try {
-            const axios = require('axios').default;
-            const secretResponse = await axios.get( req.query.source );
-
-            const ical = res.parseCSVtoIcal( secretResponse.data, req.query.headerRow, req.query.repeat )
+        const headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
+        }
+        const axios = require('axios');
+        axios.get( req.query.source, {
+            headers,
+        } ).then(response => {
+            console.log(response.data);
+            const ical = res.parseCSVtoIcal( response.data, req.query.headerRow, req.query.repeat )
 
             // Set the response status and headers
             res.writeHead(200, { 'Content-Type': 'text/plain' });
 
             // Return the content fetched from the secret URL
             res.end(ical);
-        } catch (error) {
+        })
+        .catch(error => {
+            console.error("There was an error!", error);
             res.writeHead(500, { 'Content-Type': 'text/plain' });
             res.end('Server Error');
-        }
+        });;
     },
 
 };
